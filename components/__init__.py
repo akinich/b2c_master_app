1d9e784b4227cb463a05caec354e964ccf4ac6c0
akinich <akinich@gmail.com>
2025-10-30 12:09:50 +0530

    Update __init__.py
    changed the code from 
    show_role_permissions t0 
    show_user_permissions

--- components/__init__.py
+++ components/__init__.py
@@ -5,7 +5,7 @@ from .sidebar import show_sidebar, show_module_breadcrumb
 from .dashboard import show_dashboard
 from .admin_panel import (
     show_user_management,
-    show_role_permissions,
+    show_user_permissions,
     show_activity_logs,
     show_module_management
 )
